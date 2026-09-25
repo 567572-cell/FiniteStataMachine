fc9a711c9de51b7bd85906119576b9929e9ab171
Olusegun Fayose Elliot <567572@escg.ac.uk>
2026-09-25 12:11:47 +0100

    c

---
+++ Assets/Scripts/Player States/IdleState.cs
@@ -0,0 +1,89 @@
+
+//This is a derived class of State
+//This means it inherits fields and methods from State.cs
+
+using UnityEngine;
+using System.Collections;
+
+public class IdleState : State
+{
+    // constructor
+    public IdleState( PlayerScript player, StateMachine sm) : base(player, sm)
+    {
+    }
+
+    public override void Enter()
+    {
+        // this method is called when the state begins
+
+        Debug.Log("entering idle state");
+        player.sr.color = new Color(0.5f, 0.8f, 0.7f);
+    }
+
+    public override void Exit()
+    {
+        // this method is called when the state has finished
+        Debug.Log("exiting idle state");
+
+        //you should disable any running coroutines here
+        player.StopAllCoroutines();
+    }
+
+
+    public override void Update()
+    {
+        if( player.moveAction.ReadValue<Vector2>().magnitude > 0.1f )
+        {
+            sm.ChangeState(sm.runState);
+        }
+
+        if (player.jumpAction.IsPressed())
+        {
+            sm.ChangeState(sm.jumpState);
+        }
+
+
+        //example of running a coroutine from a state and not directly from the monobehaviour
+        if (player.crouchAction.IsPressed())
+        {
+            player.StartCoroutine( IdleCo() );
+        }
+
+        UIscript.ui.DrawText("*** This is the idle state ***\n");
+        UIscript.ui.DrawText("Space = Jump State");
+        UIscript.ui.DrawText("Left/Right arrows = Move State");
+        UIscript.ui.DrawText("C = Start the coroutine");
+
+
+    }
+
+    public override void FixedUpdate()
+    {
+    }
+
+    public override void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collided");
+    }
+
+
+    public IEnumerator IdleCo()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            yield return new WaitForSeconds(2);
+            Debug.Log("Coroutine step 1");
+
+            yield return new WaitForSeconds(2);
+            Debug.Log("Coroutine step 2");
+
+            Debug.Log("Coroutine repeat " + (i+1));
+
+        }
+        yield break;
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Player States/RunState.cs
@@ -0,0 +1,90 @@
+﻿
+//This is a derived class of State
+//This means it inherits fields and methods from State.cs
+
+using UnityEngine;
+
+public class RunState : State
+{
+    protected float speed;
+    protected float rotationSpeed;
+
+    public RunState(PlayerScript player, StateMachine sm) : base(player, sm)
+    {
+    }
+
+    public override void Enter()
+    {
+        speed = 3;
+        base.Enter();
+        horizontalInput = verticalInput = 0.0f;
+
+        Debug.Log("entering running state");
+
+        player.sr.color = new Color(0.8f, 0.8f, 0.2f);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+
+
+    public override void Update()
+    {
+
+        TestMethod("hello");
+
+        
+
+        ReadInput();
+
+        if (player.interactAction.IsPressed())
+        {
+            sm.ChangeState(sm.idleState);
+        }
+
+        if (player.jumpAction.IsPressed())
+        {
+            sm.ChangeState(sm.jumpState);
+        }
+
+        //debug move gameObject
+        player.rb.linearVelocity = player.moveAction.ReadValue<Vector2>() * speed;
+
+
+        UIscript.ui.DrawText("*** This is the running state ***\n");
+        UIscript.ui.DrawText("Left/Right arrows = Move Sprite");
+        UIscript.ui.DrawText("E = Idle State");
+        UIscript.ui.DrawText("Space = Jump state");
+
+
+
+    }
+
+    public override void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collided in runstate");
+
+        if( collision.tag == "enemy")
+        {
+            collision.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0);
+        }
+    }
+    public override void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("exit collision in runstate");
+
+        if (collision.tag == "enemy")
+        {
+            collision.GetComponent<SpriteRenderer>().color = new Color(0.1f, 0.1f, 0.1f);
+        }
+    }
+
+
+
+    public override void FixedUpdate()
+    {
+    }
+}
